ea61621b37f153be3098f1fa2115b691f037177f
Micheline Ntale <ntalemicheline@gmail.com>
2025-12-21 01:52:52 +0000

    Accelère la vitesse de toutes les touches de l'application? toutes sans

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -90,8 +90,8 @@ export default {
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        'accordion-down': 'accordion-down var(--animation-duration) ease-out',
+        'accordion-up': 'accordion-up var(--animation-duration) ease-out',
       },
     },
   },
