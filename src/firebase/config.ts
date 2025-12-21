86885f98794113a66f93d9eeb8ba605610598c02
Micheline Ntale <ntalemicheline@gmail.com>
2025-12-21 00:32:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3869958785-8b335",
+  "appId": "1:668567505848:web:7d882a4ce2468b12151d4d",
+  "apiKey": "AIzaSyDfLkPL9R4Pl70Z8H4ovSX_MEFLLu6W0AI",
+  "authDomain": "studio-3869958785-8b335.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "668567505848"
+};
