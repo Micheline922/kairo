c38d3cbf1d2dad375da49a04fc367c3d3b1e503e
Micheline Ntale <ntalemicheline@gmail.com>
2025-12-21 21:56:14 +0000

    ërmets à ce que l'application soit téléchargable et après le téléchargem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { LanguageProvider } from '@/context/language-provider';
 export const metadata: Metadata = {
   title: 'KAIRO',
   description: 'C\'est le moment que Dieu a choisi pour nous parler ou pour agir. Ne laissons pas passer cette chance.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -21,6 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#5C6B73" />
       </head>
       <body className="font-headline antialiased">
         <ThemeProvider
