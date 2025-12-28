2a3d735e87a2acbd6028ce402b4b247202265798
Micheline Ntale <ntalemicheline@gmail.com>
2025-12-28 19:45:03 +0000

    L'enregistrement n'accepte pasune fois le site déployé, gère ça

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,28 +2,44 @@
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { Auth, connectAuthEmulator, getAuth } from 'firebase/auth';
+import { Firestore, connectFirestoreEmulator, getFirestore } from 'firebase/firestore'
+
+function getSdks(firebaseApp: FirebaseApp) {
+  const auth = getAuth(firebaseApp);
+  const firestore = getFirestore(firebaseApp);
+
+  if (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined' && window.location.hostname === 'localhost') {
+    // Note: You can't use process.env.NEXT_PUBLIC_... here because this is not a Next.js file.
+    // We are deliberately not using NEXT_PUBLIC_... because we don't want to expose these to the client.
+    const firestoreHost = process.env.FIRESTORE_EMULATOR_HOST || 'localhost';
+    const firestorePort = parseInt(process.env.FIRESTORE_EMULATOR_PORT || '8080', 10);
+    const authHost = process.env.AUTH_EMULATOR_HOST || 'localhost';
+    const authPort = parseInt(process.env.AUTH_EMULATOR_PORT || '9099', 10);
+    
+    console.log(`Connecting to Firestore emulator at http://${firestoreHost}:${firestorePort}`);
+    connectFirestoreEmulator(firestore, firestoreHost, firestorePort);
+
+    console.log(`Connecting to Auth emulator at http://${authHost}:${authPort}`);
+    connectAuthEmulator(auth, `http://${authHost}:${authPort}`, { disableWarnings: true });
+  }
+
+  return {
+    firebaseApp,
+    auth,
+    firestore
+  };
+}
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   if (!getApps().length) {
-    // We always initialize with the config object to ensure a stable connection.
     const firebaseApp = initializeApp(firebaseConfig);
     return getSdks(firebaseApp);
   }
-
-  // If already initialized, return the SDKs with the already initialized App
   return getSdks(getApp());
 }
 
-export function getSdks(firebaseApp: FirebaseApp) {
-  return {
-    firebaseApp,
-    auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
-  };
-}
 
 export * from './provider';
 export * from './client-provider';
